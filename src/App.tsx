f5ac204c41614d53448f8689b5e79a39aaa9e226
vladwulf <agaev.vladimir@gmail.com>
2022-01-10 12:03:33 +0000

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import zustandLogo from "./assets/zustand.logo.png";
+import easyPeasyLogo from "./assets/easy-peasy.logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <img
+            src={zustandLogo}
+            alt="zustand"
+            style={{ height: 200 }}
+          />
+          <h1>Zustand Store</h1>
+        </div>
+
+        <div style={{ marginTop: 50 }}>
+          <img
+            src={easyPeasyLogo}
+            alt="easy-peasy"
+            style={{ height: 200 }}
+          />
+          <h1>Easy Peasy Store</h1>
+        </div>
+      </div>
     </div>
   );
 }
